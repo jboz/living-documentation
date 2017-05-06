880fa5a0f86ddf144386216d7cd5cdb8bce69e33
jboz <julienboz@gmail.com>
2017-05-06 11:51:24 +0200

    move deploy settings to a dedicated branch

--- deploy/publish.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-    mvn deploy -f livingdoc-annotations/pom.xml --settings $GPG_DIR/settings.xml -DperformRelease=true -DskipTests=true
-    mvn deploy -f livingdoc-maven-plugin/pom.xml --settings $GPG_DIR/settings.xml -DperformRelease=true -DskipTests=true
-    exit $?
-fi
