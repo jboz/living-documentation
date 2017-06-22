c408ce17a5d2abb81dd3bd60817d5fceff80cce2
jboz <julienboz@gmail.com>
2017-06-23 00:55:18 +0200

    make release v0.1

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,5 +1,32 @@
 #!/bin/bash
 
+function setReleaseVersion {
+    echo "set $1 to release version"
+    if ! mvn -f $1/pom.xml -q build-helper:parse-version versions:set -DnewVersion=\${parsedVersion.majorVersion}.\${parsedVersion.minorVersion} versions:commit; then
+        echo "set release version failed"
+        exit 1
+    fi
+}
+
+function setNextDevVersion {
+    echo "set $1 to next development version"
+    if ! mvn -f $1/pom.xml -q build-helper:parse-version versions:set -DnewVersion=\${parsedVersion.majorVersion}.\${parsedVersion.nextMinorVersion}-SNAPSHOT versions:commit; then
+        echo "set next development version failed"
+        exit 1
+    fi
+}
+
+function deploy {
+    echo "deploying version of $1 to maven centrale..."
+    if ! mvn -f $1/pom.xml deploy --settings .travis/settings.xml -Prelease -DskipTests=true -B -U;then
+        echo "maven deploy failed"
+        exit 1
+    fi
+}
+
+
+
+
 # Pull requests and commits to other branches shouldn't try to deploy, just build to verify
 if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "master" ]; then
     echo "Skipping deploy; just doing a build."
@@ -11,17 +38,16 @@ git checkout master
 
 MAKE_RELEASE='false'
 
-if [[ "${TRAVIS_COMMIT_MESSAGE}" =~ ^make\ release ]]; then 
+if [[ "${TRAVIS_COMMIT_MESSAGE}" =~ ^make\ release ]]; then
     echo "commit message indicate that a release must be create"
     MAKE_RELEASE='true'
 fi
 
 if [ "$MAKE_RELEASE" = 'true' ]; then
-    echo "create release from actual SNAPSHOT"
-    if ! mvn build-helper:parse-version versions:set -DnewVersion=\${parsedVersion.majorVersion}.\${parsedVersion.minorVersion} versions:commit; then
-        echo "set release version failed"
-        exit 1
-    fi
+    # upgrade poms to final release version
+    for project in $(find . -name 'pom.xml' -printf '%h\n'); do
+        setReleaseVersion $project
+    done
 else
     echo "keep snapshot version in pom.xml"
 fi
@@ -30,11 +56,9 @@ echo "reading project version..."
 PROJECT_VERSION=`mvn -q exec:exec -Dexec.executable="echo" -Dexec.args='${project.version}' --non-recursive`
 
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" = 'false' ]; then
-    echo "deploying version to maven centrale..."
-    if ! mvn deploy --settings .travis/settings.xml -Prelease -DskipTests=true -B -U;then
-        echo "maven deploy failed"
-        exit 1
-    fi
+    # deploy this 2 artifacts only
+    deploy livingdoc-annotations
+    deploy livingdoc-maven-plugin
 fi
 
 if [ "$MAKE_RELEASE" = 'true' ]; then
@@ -45,7 +69,9 @@ if [ "$MAKE_RELEASE" = 'true' ]; then
     git tag "$GIT_TAG" -a -m "Generated tag from TravisCI for build $TRAVIS_BUILD_NUMBER"
 
     echo "preparing next version..."
-    mvn -q build-helper:parse-version versions:set -DnewVersion=\${parsedVersion.majorVersion}.\${parsedVersion.nextMinorVersion}-SNAPSHOT versions:commit
+    for project in $(find . -name 'pom.xml' -printf '%h\n'); do
+        setNextDevVersion $project
+    done
 
     NEXT_VERSION=`mvn -q exec:exec -Dexec.executable="echo" -Dexec.args='${project.version}' --non-recursive`
     echo "next development version will be $NEXT_VERSION"
@@ -53,7 +79,7 @@ if [ "$MAKE_RELEASE" = 'true' ]; then
     # Commit the "changes", i.e. the new version.
     git add -A .
     git commit -m "set next development version to $NEXT_VERSION"
-    
+
     REPO=`git config remote.origin.url`
     echo "pushing new development version..."
     git push --tags "https://$GITHUB_TOKEN@github.com/$TRAVIS_REPO_SLUG.git"
