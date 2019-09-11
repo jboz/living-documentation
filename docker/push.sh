92cf0c086951f49c9a520159e7b81ac617cffe1c
Julien Boz <julienboz@gmail.com>
2019-09-11 20:21:07 +0200

    upgrade maven version

--- docker/push.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker push jboz/maven-graphviz
--- docker/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-docker run -it --rm \
-    -v "$HOME/.m2":/root/.m2 \
-    -v "$(pwd)":/usr/src/mymaven \
-    -v "$(pwd)/target:/usr/src/mymaven/target" \
-    -w /usr/src/mymaven \
-    jboz/maven-graphviz \
-    mvn $1
