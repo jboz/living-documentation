ec40d5e7d63428e79cf877e41cb6f2998c27ea58
Julien Boz <julienboz@gmail.com>
2019-09-13 15:14:40 +0200

    Saving files before refreshing line endings

--- livingdoc-maven-plugin/src/it/ddd/verify.groovy
+++ livingdoc-maven-plugin/src/it/ddd/verify.groovy
@@ -1,10 +1,10 @@
-new File(basedir, "./src/test/resources/expected").eachFileMatch(~/^.*\.(adoc|html|plantuml|svg)$/) { file ->
+new File(basedir, "./src/test/resources").eachFileMatch(~/^.*\.(adoc|html|plantuml|svg)$/) { file ->
     println "Assert generated file " + file.name + " to exists"
 
     assert new File(basedir, "target/" + file.name).isFile()
 }
 
-new File(basedir, "./src/test/resources/expected").eachFileMatch(~/^.*\.(adoc|plantuml)$/) { file ->
+new File(basedir, "./src/test/resources").eachFileMatch(~/^.*\.(adoc|plantuml)$/) { file ->
     println "Assert generated file to equals expected: " + file.name
 
     assert new File(basedir, "target/" + file.name).text == file.text
