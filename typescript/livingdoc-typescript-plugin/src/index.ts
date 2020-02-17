9f8f19356977635c4e497154570fc6cdfdbf9582
Julien Boz <julienboz@gmail.com>
2020-02-17 22:03:25 +0100

    command line !

---
+++ typescript/livingdoc-typescript-plugin/src/index.ts
@@ -0,0 +1,22 @@
+import program from 'commander';
+import G from 'glob';
+import { diagram } from './diagram.mojo';
+import { exportDocument } from './export.puml';
+
+program
+  .version('0.0.1')
+  .option('-i, --input <path>', 'Define the path of the Typescript file')
+  .option('-o, --output <path>', "Define the path of the output file. If not defined, it'll output on the STDOUT")
+  .option('-d, --deep <boolean>', 'Indicate if program must go throw depencies content or not', true);
+
+program.parse(process.argv);
+
+if (!program.input) {
+  console.error('Missing input file');
+  process.exit(1);
+}
+
+G(<string>program.input, (err: Error | null, matches: string[]) => {
+  const document = diagram.generateDiagram(matches, program.deep);
+  exportDocument(document, program.output);
+});
