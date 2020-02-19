cd325707f57533c12472b8c5a6414d5d1bfa4851
jboz <julienboz@gmail.com>
2020-02-20 00:24:26 +0100

    npm: upgrade minor version if required and push tag

--- livingdoc-typescript-plugin/src/index.ts
+++ livingdoc-typescript-plugin/src/index.ts
@@ -5,7 +5,7 @@ import { exportDocument } from './export.puml';
 
 program
   .version(require('../package.json').version, '-v, --version', 'output the current version')
-  .option('-i, --input <path>', 'define the path of the Typescript file')
+  .option('-i, --input <path>', 'define the path of the typescript file')
   .option('-o, --output <path>', "define the path of the output file. If not defined, it'll output on the STDOUT")
   .option('-d, --deep <boolean>', 'indicate if program must through dependancies content or not', true);
 
