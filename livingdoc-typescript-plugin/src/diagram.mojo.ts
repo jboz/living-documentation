8b32dada5e1727f1f5bd32ab6937a3769987bf7d
jboz <julienboz@gmail.com>
2020-02-20 23:37:17 +0100

    comment (#33)

--- livingdoc-typescript-plugin/src/diagram.mojo.ts
+++ livingdoc-typescript-plugin/src/diagram.mojo.ts
@@ -3,6 +3,7 @@ import { ClassDiagramBuilder } from './diagram/class-diagram.builder';
 
 export namespace diagram {
   export const generateDiagram = (roots: ReadonlyArray<string>, deep = true) => {
+    // if true, only reference of dependencies will be showned, without content
     const singleFile = !deep && roots.length === 1; // && !fs.lstatSync(roots[0]).isDirectory();
     const program = createProgram(roots, {});
     const diagram = new ClassDiagramBuilder(program.getTypeChecker())
